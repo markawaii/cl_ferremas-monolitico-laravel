427e7ce92a9f1d6edf16ade90465a4d5ab525b39
Ignacio Veliz <101842633+MarKawaii@users.noreply.github.com>
2024-05-05 16:46:22 -0400

    Update api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -5,10 +5,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(ProductoController::class)->group(function () {
-    Route::post('/producto/crear-producto', 'crearProducto');
-    Route::delete('/producto/eliminar-producto', 'eliminarProducto');
-    Route::put('/producto/cambiar-precio', 'cambiarPrecio');
-    Route::get('/producto/obtener-producto', 'obtenerProducto');
-    Route::put('/producto/modificar-producto', 'modificarProducto');
     Route::get('/producto/listar-todos-los-productos', 'listarTodosLosProductos');
+    Route::get('/producto/obtener-producto', 'obtenerProducto');
+    Route::post('/producto/crear-producto', 'crearProducto');
+    Route::post('/producto/nuevo-precio-de-producto', 'nuevoPrecio');
+    Route::put('/producto/modificar-producto', 'modificarProducto');
+    Route::delete('/producto/eliminar-producto', 'eliminarProducto');
 });
