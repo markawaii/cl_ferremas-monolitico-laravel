cc860befc9b1d685e21d2757117e269216581cd9
Ignacio Veliz <101842633+MarKawaii@users.noreply.github.com>
2024-06-23 05:07:11 -0400

    feat: logo

--- laravel/resources/views/layouts/navbars/auth/sidenav.blade.php
+++ laravel/resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -3,9 +3,9 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href=""
+        <a class="navbar-brand m-0 ms-5" href=""
             target="_blank">
-            <img src="./img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="{{ asset('assets/img/ferreteria.png') }}" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">Ferremas</span>
         </a>
     </div>
